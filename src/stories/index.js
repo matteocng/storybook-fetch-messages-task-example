08bd9dbb4797b8426204d5a35672a3c351f39687
matteocng <matteocng@users.noreply.github.com>
2017-11-01 14:25:25 +0100

    chore: add FormattedMessage story to storybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,8 +1,10 @@
+// @flow
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { FormattedMessage } from 'react-intl';
 
 import { Button, Welcome } from '@storybook/react/demo';
 
@@ -10,4 +12,7 @@ storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo(
 
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>)
+  .add('with FormattedMessage', () =>
+    <Button onClick={action('clicked')}><FormattedMessage id="app.demo" /></Button>
+  )
